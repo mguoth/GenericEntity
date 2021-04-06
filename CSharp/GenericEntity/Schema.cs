6a872269600dea6fa34db2662a7d1a25219a4347
Miroslav Gouth <miroslav.guoth@pdisoftware.com>
2021-04-06 23:04:07 +0200

    use alias

--- CSharp/GenericEntity/Schema.cs
+++ CSharp/GenericEntity/Schema.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 using GenericEntity.Abstractions;
+using SchemaModel = Schema.Model;
+
 
 namespace GenericEntity
 {
     public class Schema : ISchema
     {      
-        private readonly global::Schema.Model.SchemaDefinition data;
+        private readonly SchemaModel.SchemaDefinition data;
 
-        public Schema(global::Schema.Model.SchemaDefinition data, SchemaCompilerData compilerData)
+        public Schema(SchemaModel.SchemaDefinition data, SchemaCompilerData compilerData)
         {
             this.data = data;
             this.Fields = this.data.Fields.Select(x => new FieldDefinition(this, compilerData.FieldTypes[x.Type], x)).ToArray();

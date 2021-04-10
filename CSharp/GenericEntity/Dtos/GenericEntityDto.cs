75a58c577e45b67a52d2e483a03e42cb9da07ef8
Miroslav Gouth <miroslav.guoth@pdisoftware.com>
2021-04-10 20:43:04 +0200

    Add generic entity DTO

---
+++ CSharp/GenericEntity/Dtos/GenericEntityDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace GenericEntity
+{
+
+    public class GenericEntityDto
+    {
+        private IDictionary<string, object> fields = new Dictionary<string, object>();
+
+        public string SchemaName { get; set; }
+
+        [JsonConverter(typeof(FieldsConverter))]
+        public IDictionary<string, object> Fields { get => fields; set => fields = value; }
+    }
+}

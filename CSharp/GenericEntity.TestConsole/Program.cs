f8127ad36df1d1eae369fea32b28cef6ec9c63ae
Miroslav Gouth <miroslav.guoth@pdisoftware.com>
2021-12-05 17:49:16 +0100

    Adding details into program.cs

--- CSharp/GenericEntity.TestConsole/Program.cs
+++ CSharp/GenericEntity.TestConsole/Program.cs
@@ -25,18 +25,38 @@ namespace GenericEntity.CLI
             address.Fields["postalCode"].SetString("10030");
             address.Fields["country"].SetString("US");
 
+            Console.WriteLine($"Creating generic entity instance.");
+            Console.WriteLine();
+
+            Print(address);
+
+            Console.WriteLine($"Serialising it into JSON.");
+            Console.WriteLine();
+
             //Serialise generic entity into Json
             string json = JsonSerializer.Serialize(address, new JsonSerializerOptions() { WriteIndented = true });
+            
+            Console.WriteLine(json);
+            Console.WriteLine();
+
+            Console.WriteLine($"Deserialising it back into generic entity.");
+            Console.WriteLine();
 
             //Deserialise generic entity from Json
             GenericEntity reconstructedAddress = JsonSerializer.Deserialize<GenericEntity>(json);
 
+            Print(reconstructedAddress);
+        }
+
+        private static void Print(GenericEntity genericEntity)
+        {
             //Enumerating fields and getting value as string (to string conversion is supported by all field types)
-            Console.WriteLine($"{address.Schema.EntityType} {nameof(address)} ({address.GetType()})");
-            foreach (IField field in reconstructedAddress.Fields)
+            Console.WriteLine($"{genericEntity.Schema.EntityType} entity ({genericEntity.GetType()})");
+            foreach (IField field in genericEntity.Fields)
             {
                 Console.WriteLine($"|- {field.Definition.Type} {field.Definition.Name}: {field.GetString()} ({field.DataType})");
             }
+            Console.WriteLine();
         }
     }
 }

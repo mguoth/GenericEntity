b7dea12332f01d8e11bada8569fa73b9458f6ef4
Miroslav Guoth <cooltan01@gmail.com>
2023-01-08 17:49:55 +0100

    Add tests

--- CSharp/Tests/GenericEntity.Tests/FunctionalTests.cs
+++ CSharp/Tests/GenericEntity.Tests/FunctionalTests.cs
@@ -9,60 +9,110 @@ namespace GenericEntity.Tests
 {
     public class FunctionalTests
     {
-        [Fact]
-        public void JsonSerialisation()
+        //Initialization of the schema repository
+        private static readonly ISchemaRepository schemaRepository = new InMemorySchemaRepository()
         {
-            //Initialization of the schema repository
-            ISchemaRepository schemaRepository = new InMemorySchemaRepository()
             {
-                {
-                    "address",
-                    "avsc",
-                    @$"{{
-                        ""type"": ""record"",
-                        ""name"": ""Address"",
-                        ""namespace"": ""GenericEntity.Samples"",
-                        ""fields"": [
-                        {{
-                            ""name"": ""id"",
-                            ""type"": ""int"",
-                            ""displayName"": ""Id"",
-                            ""doc"": ""The unique identifier""
-                        }},
-                        {{
-                            ""name"": ""addressLine1"",
-                            ""type"": ""string"",
-                            ""displayName"": ""Address line 1""
-                        }},
-                        {{
-                            ""name"": ""addressLine2"",
-                            ""type"": ""string"",
-                            ""displayName"": ""Address line 2""
-                        }},
-                        {{
-                            ""name"": ""city"",
-                            ""type"": ""string"",
-                            ""displayName"": ""City""
-                        }},
-                        {{
-                            ""name"": ""postalCode"",
-                            ""type"": ""string"",
-                            ""displayName"": ""Postal code""
-                        }},
-                        {{
-                            ""name"": ""country"",
-                            ""type"": ""string"",
-                            ""displayName"": ""Country""
-                        }}
-                        ]
-                    }}"
-                }
-            };
+                "address",
+                "avsc",
+                @$"{{
+                    ""type"": ""record"",
+                    ""name"": ""Address"",
+                    ""namespace"": ""GenericEntity.Samples"",
+                    ""fields"": [
+                    {{
+                        ""name"": ""id"",
+                        ""type"": ""int"",
+                        ""displayName"": ""Id"",
+                        ""doc"": ""The unique identifier""
+                    }},
+                    {{
+                        ""name"": ""addressLine1"",
+                        ""type"": ""string"",
+                        ""displayName"": ""Address line 1""
+                    }},
+                    {{
+                        ""name"": ""addressLine2"",
+                        ""type"": ""string"",
+                        ""displayName"": ""Address line 2""
+                    }},
+                    {{
+                        ""name"": ""city"",
+                        ""type"": ""string"",
+                        ""displayName"": ""City""
+                    }},
+                    {{
+                        ""name"": ""postalCode"",
+                        ""type"": ""string"",
+                        ""displayName"": ""Postal code""
+                    }},
+                    {{
+                        ""name"": ""country"",
+                        ""type"": ""string"",
+                        ""displayName"": ""Country""
+                    }}
+                    ]
+                }}"
+            }
+        };
 
+        static FunctionalTests()
+        {
             //Adding generic entity extensions
             GenericEntity.Extensions.AddStandard();
             GenericEntity.Extensions.AddAvro();
+        }
 
+        [Fact]
+        public void GetField_Exists_ReturnField()
+        {
+            //Creating address entity
+            SchemaInfo schemaInfo = schemaRepository.GetSchema("address");
+            GenericEntity address = new GenericEntity(schemaInfo, GenericEntity.Extensions.GetSchemaParser(schemaInfo.Format));
+
+            IField field = address.Fields["id"];
+            Assert.NotNull(field);
+            Assert.Equal("id", field.Name, ignoreCase: true);
+        }
+
+        [Fact]
+        public void GetField_NotExists_ThrowException()
+        {
+            //Creating address entity
+            SchemaInfo schemaInfo = schemaRepository.GetSchema("address");
+            GenericEntity address = new GenericEntity(schemaInfo, GenericEntity.Extensions.GetSchemaParser(schemaInfo.Format));
+
+            Assert.Throws<KeyNotFoundException>(() => address.Fields["nonExistingField"]);
+        }
+
+        [Fact]
+        public void TryGetField_Exists_ReturnField()
+        {
+            //Creating address entity
+            SchemaInfo schemaInfo = schemaRepository.GetSchema("address");
+            GenericEntity address = new GenericEntity(schemaInfo, GenericEntity.Extensions.GetSchemaParser(schemaInfo.Format));
+
+            bool result = address.Fields.TryGetField("id", out IField field);
+            Assert.True(result);
+            Assert.NotNull(field);
+            Assert.Equal("id", field.Name, ignoreCase: true);
+        }
+
+        [Fact]
+        public void TryGetField_NotExists_ReturnNull()
+        {
+            //Creating address entity
+            SchemaInfo schemaInfo = schemaRepository.GetSchema("address");
+            GenericEntity address = new GenericEntity(schemaInfo, GenericEntity.Extensions.GetSchemaParser(schemaInfo.Format));
+
+            bool result = address.Fields.TryGetField("nonExistingField", out IField field);
+            Assert.False(result);
+            Assert.Null(field);
+        }
+
+        [Fact]
+        public void JsonSerialisation()
+        {
             //Creating address entity
             SchemaInfo schemaInfo = schemaRepository.GetSchema("address");
             GenericEntity address = new GenericEntity(schemaInfo, GenericEntity.Extensions.GetSchemaParser(schemaInfo.Format));
@@ -76,7 +126,7 @@ namespace GenericEntity.Tests
             //Serialise
             string addressJson = JsonSerializer.Serialize(address);
 
-            GenericEntity reconstructedAddress = JsonSerializer.Deserialize<GenericEntity>(addressJson);
+            GenericEntity? reconstructedAddress = JsonSerializer.Deserialize<GenericEntity>(addressJson);
 
             string reconstructedAddressJson = JsonSerializer.Serialize(reconstructedAddress);
 
